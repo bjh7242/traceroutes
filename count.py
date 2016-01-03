79f5392b72d5ff8c30e061efab04b48047141697
bjh7242 <bjh7242@rit.edu>
2016-01-03 03:55:37 +0000

    adding comment explaining bash command

--- count.py
+++ count.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 #cat trace| grep --no-group-separator -B 1 "traceroute to" | grep -v "traceroute to" | grep -vF "30  * * *" | awk '{ print $1}'
 
+count = 0
+# numhosts is the number of domains returning a definite traceroute
+# the commented command on line 2 should be used to remove all traceroutes that do not fully complete (ex. if
+# a host is blocking ICMP port unreachable messages)
+numhosts = 0
 
+with open('hops','r') as f:
+    for num in f:
+        count += int(num)
+        numhosts += 1
+
+#print count
+#print numhosts
+print "The average number of hops is: " + str(float(count)/float(numhosts))
